6b08ace63f8deef7e023fa96c3585bfe68eb856c
haruperi <r.haruperi@hotmail.com>
2022-03-15 09:08:08 +0200

    Data Types and Variables

--- assets/js/arrays.js
+++
@@ -1,8 +0,0 @@
-let demo = document.querySelector('#demo')
-let cars = ['benz','bmw','toyota']
-cars[100] ='fox'
-
-let vowels = 'aeiou'.toArray()
-
-va
-console.log(vowels)
---
+++ assets/js/js1-datatypes-variables.js
@@ -0,0 +1,186 @@
+// 1.0 JAVASCRIPT TYPES
+
+//1.1 Numbers
+let number = 23
+
+//1.2 Strings
+const firstName = "Rufaro"
+
+//1.3 Boolean
+const isMarried = true
+
+//1.4 Undefined
+let varName1
+
+//1.5 Null
+let varName2  = null
+
+//1.6 Symbol
+let varSymbol  = Symbol("id")
+
+//1.7 BigInt
+const hugeNumber = BigInt(9007199254740991)
+
+//1.8 Objects
+let userObject  = {
+    userName : "Rufaro",
+    age : 33
+};
+
+// Check type of variable
+let checkType = typeof isMarried
+
+// 2.0 JAVASCRIPT COMPARISON
+// 3!=4
+// 3===3
+// 3>2
+// 3<4
+// 3>=2
+// 3<=4
+
+// 3.0 JAVASCRIPT VARIABLES
+const constNumber = 1988
+let changebleNumber = 10
+
+
+
+
+/**********************************************************************************
+ *
+ * EXERCISES
+ *
+ * *******************************************************************************/
+
+
+/*
+Values and Variables
+------------------------
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+ */
+
+
+/*
+Data Types
+------------------------
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+ */
+
+/*
+let, const and var
+--------------------------
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+ */
+
+
+/*
+Basic Operators
+-----------------------------------
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+*/
+
+/*
+  Write an expression that uses at least three, different, arithmetic operators
+  to log the number 42 to the console.
+ */
+
+
+/*
+  The Celsius-to-Fahrenheit formula:
+     F = C x 1.8 + 32
+
+  1. Set the fahrenheit variable to the correct value using the celsius variable and the forumla above
+  2. Log the fahrenheit variable to the console
+ */
+
+/*
+Strings and Template Literals
+----------------------------------
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax
+ */
+
+/*
+Build a single string that resembles exactly the following (Each string should be printed on its own line).
+
+1. 'Why couldn't the shoes go out and play?
+They were all "tied" up!'
+
+2. 'Blowing from the west
+Fallen leaves gather
+In the east.'
+ */
+
+
+/*
+Fix the right side expression so it evaluates to true.
+let answer = "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal";
+ */
+
+/*
+Create a variable called bill and assign it the result of 10.25 + 3.99 + 7.15 (don't perform the calculation yourself, let JavaScript do it!).
+Next, create a variable called tip and assign it the result of multiplying bill by a 15% tip rate.
+Finally, add the bill and tip together and store it into a variable called total.
+
+Print the total to the JavaScript console.
+
+Hint: 15% in decimal form is written as 0.15_._
+
+TIP: To print out the total with a dollar sign ( $ ) use string concatenation. To round total up by two decimal points use the toFixed() method.
+To use toFixed() pass it the number of decimal points you want to use. For example, if total equals 3.9860, then total.toFixed(2) would return 3.99.
+ */
+
+/*
+Mad Libs is a word game where players have fun substituting words for blanks in a story.
+
+"The Intro to JavaScript course is __________. James and Julia are so __________. I cannot wait to work through the rest of this __________ content!"
+
+  1. Declare a madLib variable
+  2. Use the adjective1, adjective2, and adjective3 variables to set the madLib variable to the message:
+
+  'The Intro to JavaScript course is amazing. James and Julia are so fun. I cannot wait to work through the rest of this entertaining content!'
+ */
+
+
+
+/*
+ * Programming Quiz: One Awesome Message
+
+  1. Create the following variables:
+      - firstName
+      - interest
+      - hobby
+  2. Create a variable named awesomeMessage and set it to an awesome message using
+      string concatenation and the variables above.
+  3. Log the awesomeMessage variable to the console.
+
+  Notes:
+  - The `awesomeMessage` should have the format of:
+    Hi, my name is _____. I love _____. In my spare time, I like to _______.
+
+  - Using the above as an example, firstName would have been assigned to "Julia",
+    interest to "cats", and hobby to "play video games" to produce the message:
+    Hi, my name is Julia. I love cats. In my spare time, I like to play video games.
+
+  - Be sure to include spaces and periods where necessary!
+ */
+
+
